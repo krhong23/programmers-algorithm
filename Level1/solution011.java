eb6640bf2874cbdb1ded11d4114452157e958fa7
krhong <krhong@iteyes.co.kr>
2021-05-17 18:14:05 +0900

    프로그래머스 코딩 테스트 Level1 - 011

---
+++ Level1/solution011.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] solution01(int[] arr, int divisor) {
+        List<Integer> answer = new ArrayList<Integer>();
+
+        for (int i : arr) {
+            if (i % divisor == 0) answer.add(i);
+        }
+
+        if (answer.size() < 1) answer.add(-1);
+        answer.sort(null);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public int[] solution02(int[] arr, int divisor) {
+        int[] answer = Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray();
+        if (answer.length == 0) answer = new int[]{-1};
+        Arrays.sort(answer);
+        return answer;
+    }
+
+}
